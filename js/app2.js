0af31fe262375496208a1118e9d397a3148ac17d
00be <66236573+00be@users.noreply.github.com>
2023-03-26 23:17:49 -0300

    Add files via upload

---
+++ js/app2.js
@@ -0,0 +1,14 @@
+// Creamos el botón
+var boton = document.createElement("button");
+var boton2 = document.createElement("input");
+
+// Agregamos un texto al botón
+boton.innerHTML = "Haz clic aquí";
+boton2.innerHTML = "pon tu nombre";
+// Agregamos una clase al botón para modificar su estilo
+boton.classList.add("mi-clase");
+boton.classList.add("mi-clase");
+
+// Agregamos el botón al DOM
+document.body.appendChild(boton);
+document.body.appendChild(boton);
